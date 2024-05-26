d09b7644e8dcf9ad0df89fecd5c1cbfbb11b9b13
sergiofernandezv <sergiofernandezvfans@outlook.com>
2024-05-26 20:35:07 +0200

    links

--- src/App.jsx
+++ src/App.jsx
@@ -15,19 +15,19 @@ function App() {
         </div>
         <div className="social-media">
           <a href="https://x.com/Sergiofernzv?mx=2">
-            <img src="/assets/x.jpg" alt="Twitter" />
+            <img src="assets/x.jpg" alt="Twitter" />
           </a>
           <a href="https://www.instagram.com/sergiofernandezv/?hl=es">
-            <img src="/assets/instagram.jpg" alt="Instagram" />
+            <img src="assets/instagram.jpg" alt="Instagram" />
           </a>
           <a href="https://t.me/sergiofernandezvoficial">
-            <img src="/assets/telegram.jpg" alt="Telegram" />
+            <img src="assets/telegram.jpg" alt="Telegram" />
           </a>
           <a href="https://www.tiktok.com/@sergiofernzv?is_from_webapp=1&sender_device=pc">
-            <img src="/assets/tiktok.jpg" alt="TikTok" />
+            <img src="assets/tiktok.jpg" alt="TikTok" />
           </a>
           <a href="https://onlyfans.com/sergiofernandezv">
-            <img src="/assets/only.png" alt="Only" />
+            <img src="assets/only.png" alt="Only" />
           </a>
         </div>
       </div>
